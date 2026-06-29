2c16a555a06c829b5028d530977f0d8e3e3fbc34
AbrahamDF90 <abrahamdanilofernandez@gmail.com>
2026-06-29 12:53:22 -0300

    Version 2 con vistas, modelos,controlador,factory,seeders

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Primero arrancamos con el proyecto tienda de mascotas, agregaremos la base de datos, haremos las migraciones, los controladores, rutas y controlaremos comandos con postman.
+
+En segundo lugar creamos la migracion pet, editamos el up, migramos, editamos el modelo pet, editamos el controlador, registramos la ruta, creamos la vista create.blade e index.blade, finalmente creamos el factory y seeders para editarlos para la ocasion.
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PetController; 
+use Illuminate\Support\Facades\Route; 
+ 
+Route::get('/mascotas', [PetController::class, 'index'])->name('pets.index'); 
+Route::get('/mascotas/crear', [PetController::class, 'create'])->name('pets.create'); 
+Route::post('/mascotas', [PetController::class, 'store'])->name('pets.store'); 
